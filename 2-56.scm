e855024569bffc067f2ad0fe30e29977a293681f
zenthon <946021566@qq.com>
2017-05-19 00:04:25 +0800

    2017-05-19

--- 2-56.scm
+++ 2-56.scm
@@ -41,3 +41,13 @@
         (else (list '** base n))
   )
 )
+
+;;; =======================
+;;; 2.57
+;;; =======================
+(define (augend s)
+  (accumulate make-sum 0 (cddr s))
+)
+(define (multiplicand p)
+  (accumulate make-product 1 (cddr p))
+)
